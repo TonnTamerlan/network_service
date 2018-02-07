a29af9bbe891caa2540d8fa609901377cbd72aab
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-02-07 21:29:57 +0200

    add dbService.dataSets.Role version 1.0

--- src/main/java/dbService/dataSets/Role.java
+++ src/main/java/dbService/dataSets/Role.java
@@ -4,23 +4,18 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Class contains information about role of user
  * 
  * @author Alexey Kopylov
  *
+ *@version 1.0
  */
 
 @Entity
-@Table(name = "role")
+@Table(name = "roles")
 public class Role implements Serializable {
 
 	private static final long serialVersionUID = 1531665300196850154L;
@@ -29,15 +24,63 @@ public class Role implements Serializable {
 	@Column(name = "role_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long roleId;
-	
-	@Column(name="name")
+
+	@Column(name = "name", nullable = false, unique = true, updatable = false)
 	private String name;
-	
-	@OneToMany(mappedBy = "role")
+
+	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
 	private Set<User> users = new HashSet<>();
 
-	protected Role() {
+	public long getRoleId() {
+		return roleId;
 	}
 
-	
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + (int) (roleId ^ (roleId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (roleId != other.roleId)
+			return false;
+		return true;
+	}
+
 }
