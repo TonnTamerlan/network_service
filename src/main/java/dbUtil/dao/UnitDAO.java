79ee2482ab65bf87a7396d33aa5ede03c46881e5
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-02-15 22:05:44 +0200

    add interface UnitDAO for working with entity Unit

--- src/main/java/dbUtil/dao/UnitDAO.java
+++ src/main/java/dbUtil/dao/UnitDAO.java
@@ -1,18 +1,106 @@
 package dbUtil.dao;
 
+import java.util.Set;
+
 import dbUtil.dataSets.Division;
+import dbUtil.dataSets.Equipment;
 import dbUtil.dataSets.Unit;
 
+/**
+ * Interface for working with repository using entity {@link Unit}
+ * 
+ * @author Alexey Kopylov
+ *
+ */
 public interface UnitDAO {
 	
+	/**
+	 * add unit to repository
+	 * 
+	 * @param user
+	 */
 	public void addUnit(Unit unit);
 	
-	public void getUnitByID(long id);
 	
-	public void getUnitByNameAndDevision(String nameUnit, Division division);
+	/**
+	 * Find and read unit from repository by id
+	 * 
+	 * @param id is the primary key
+	 * @return unit
+	 */
+	public Unit getUnitByID(long id);
 	
+	/**
+	 * Find and read unit by unit's name and a division
+	 * 
+	 * @param nameUnit is unit's name
+	 * @param is division which the unit belongs
+	 * @return unit
+	 */
+	public Unit getUnitByNameAndDevision(String nameUnit, Division division);
+	
+	/**
+	 * Find and read unit by unit's name and a division
+	 * 
+	 * @param nameUnit is unit's name
+	 * @param nameDivision is name of division which the unit belongs
+	 * @return unit
+	 */
+	public Unit getUnitByNameAndDevision(String nameUnit, String nameDivision);
+	
+	/**
+	 * Return all of units from repository
+	 * 
+	 * @return Set of all units
+	 */
+	public Set<Unit> getAllUnits();
+	
+	/**
+	 * Get all equipments of the Unit which name is nameUnit and belongs division
+	 * 
+	 * @param nameUnit is unit's name
+	 * @param division is division which the unit belongs
+	 * @return all the unit's equipments
+	 */
+	public Set<Equipment> getEquipments(String nameUnit, Division division);
+	
+	/**
+	 * Get all equipments of the Unit which name is nameUnit and belongs division which name is nameDevision
+	 * 
+	 * @param nameUnit is unit's name
+	 * @param nameDivision is name of division which the unit belongs
+	 * @return all the unit's equipments
+	 */
+	public Set<Equipment> getEquipments(String nameUnit, String nameDivision);
+	
+	/**
+	 * Update unit's information
+	 * 
+	 * @param unit
+	 */
 	public void updateUnit(Unit unit);
 	
+	/**
+	 * Find and delete user
+	 * 
+	 * @param unit
+	 */
 	public void deleteUnit(Unit unit);
+	
+	/**
+	 * Find and delete user which name is nameUnit and belongs the division
+	 * 
+	 * @param nameUnit is unit's name
+	 * @param division is division which the unit belongs
+	 */
+	public void deleteUnit(String nameUnit, Division division);
+	
+	/**
+	 * Find and delete user which name is nameUnit and belongs division which name is nameDevision
+	 * 
+	 * @param nameUnit is unit's name
+	 * @param nameDivision is name of division which the unit belongs
+	 */
+	public void deleteUnit(String nameUnit, String nameDivision);
 	
 }
