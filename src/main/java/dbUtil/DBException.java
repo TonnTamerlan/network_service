341cb18a33c95296a9fd9272025ee3f29a42298c
Alexey <Alexey@AM-N-KOPYLOV>
2018-02-16 15:44:50 +0200

    Update DBException: add constructor

--- src/main/java/dbUtil/DBException.java
+++ src/main/java/dbUtil/DBException.java
@@ -1,7 +1,15 @@
 package dbUtil;
 
 public class DBException extends Exception {
-
+	
 	private static final long serialVersionUID = 1892107180801376600L;
+
+	public DBException(String message) {
+		super(message);
+	}
+	
+	public DBException(String message, Exception e) {
+		super(message, e);
+	}
 
 }
