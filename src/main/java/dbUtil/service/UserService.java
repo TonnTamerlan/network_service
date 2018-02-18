c46dcc33ee2637417106c7cbd9c592b05f146525
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-02-19 00:01:53 +0200

    added realizations some methods in UserService but class still needs to be modify

--- src/main/java/dbUtil/service/UserService.java
+++ src/main/java/dbUtil/service/UserService.java
@@ -1,16 +1,30 @@
 package dbUtil.service;
 
-import java.util.Set;
+import java.util.List;
 
-import org.hibernate.HibernateException;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import dbUtil.DBException;
 import dbUtil.dao.UserDAO;
 import dbUtil.dataSets.Division;
+import dbUtil.dataSets.Role;
 import dbUtil.dataSets.User;
+import dbUtil.dataSets.User_;
 
+/**
+ * Implementation interface UserDAO for working with table "users" in database
+ * 
+ * @author Alexey Kopylov
+ *
+ */
 public class UserService implements UserDAO {
 
 	final private SessionFactory SESSION_FACTORY;
@@ -23,34 +37,97 @@ public class UserService implements UserDAO {
 	public void add(User user) throws DBException {
 		try (Session session = SESSION_FACTORY.openSession()){
 			session.beginTransaction();
-			session.save(user);
+			session.persist(user);
 			session.getTransaction().commit();
+		} catch (PersistenceException e) {
+			// TODO: add logging
+			throw e;
 		} catch (Exception e) {
 			// TODO: add logging
-			throw new DBException("Cannot add User login: " + user.getLogin(), e);
+			// TODO: add processing case when the user already exists
+			throw new DBException("Cannot add User with login: " + user.getLogin(), e);
 		}
 	}
 
 	@Override
 	public User getByID(long id) throws DBException{
-		// TODO Auto-generated method stub
-		return null;
+		User user = new User();
+		try(Session session = SESSION_FACTORY.openSession()){
+			session.beginTransaction();
+			session.load(user, id);
+			user.getDivisions().size(); //for attaching the set of divisions
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// TODO: add logging
+			throw new DBException("Cannot read the user with id: " + id, e);
+		}
+		return user;
 	}
 
 	@Override
 	public User getByLogin(String login) throws DBException{
-		// TODO Auto-generated method stub
-		return null;
+		User user = null;
+		try(Session session = SESSION_FACTORY.openSession()){
+			session.beginTransaction();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
+			Root<User> userRoot = criteriaQuery.from(User.class);
+			criteriaQuery.select(userRoot);
+			Predicate predicate = builder.equal(userRoot.get(User_.login), login);
+			criteriaQuery.where(predicate);
+			user = session.createQuery(criteriaQuery).getSingleResult();
+			user.getDivisions().size(); //for attaching the set of divisions
+			session.getTransaction().commit();
+		} catch (NoResultException e) {
+			// TODO: add logging in UserService.getByLogin()
+			user = null;
+		} catch (Exception e) {
+			// TODO: add logging in UserService.getByLogin()
+			throw new DBException("Cannot read the user with login: " + login, e);
+		}
+		return user;
 	}
 
 	@Override
-	public Set<User> getAll() throws DBException{
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> getAllLogins() throws DBException{
+		List<String> userList = null;
+		try (Session session = SESSION_FACTORY.openSession()){
+			session.beginTransaction();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<String> criteriaQuery = builder.createQuery(String.class);
+			Root<User> userRoot = criteriaQuery.from(User.class);
+			criteriaQuery.select(userRoot.get(User_.login));
+			userList = session.createQuery(criteriaQuery).getResultList();
+			session.getTransaction().commit();
+		}catch (Exception e) {
+			// TODO: add logging in UserService.getAllLogins()
+			throw new DBException("Cannot read logins of all users", e);
+		}
+		return userList;
+	}
+	
+	@Override
+	public List<String> getByRole(Role role) throws DBException {
+		List<String> userList = null;
+		try (Session session = SESSION_FACTORY.openSession()){
+			session.beginTransaction();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<String> criteriaQuery = builder.createQuery(String.class);
+			Root<User> userRoot = criteriaQuery.from(User.class);
+			criteriaQuery.select(userRoot.get(User_.login));
+			Predicate predicate = builder.equal(userRoot.get(User_.role), role);
+			criteriaQuery.where(predicate);
+			userList = session.createQuery(criteriaQuery).getResultList();
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// TODO: add logging in UserService.getByRole()
+			throw new DBException("Cannot read users with role: " + role.name(), e);
+		}
+		return userList;
 	}
 
 	@Override
-	public Set<User> getUsersFromDevision(String devisionName) throws DBException{
+	public List<String> getByDevision(String devisionName) throws DBException{
 		// TODO Auto-generated method stub
 		return null;
 	}
