5ab1f17226669dae872f247478d6c86bc3209c15
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-02-20 18:27:22 +0200

    UserService: added some realization of methods, but class still needs to modify

--- src/main/java/dbUtil/service/UserService.java
+++ src/main/java/dbUtil/service/UserService.java
@@ -1,20 +1,25 @@
 package dbUtil.service;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceException;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import dbUtil.DBException;
 import dbUtil.dao.UserDAO;
 import dbUtil.dataSets.Division;
+import dbUtil.dataSets.Division_;
 import dbUtil.dataSets.Role;
 import dbUtil.dataSets.User;
 import dbUtil.dataSets.User_;
@@ -34,28 +39,33 @@ public class UserService implements UserDAO {
 	}
 	
 	@Override
-	public void add(User user) throws DBException {
+	public boolean add(User user) throws DBException {
+		boolean result = false;
 		try (Session session = SESSION_FACTORY.openSession()){
 			session.beginTransaction();
 			session.persist(user);
 			session.getTransaction().commit();
+			result = true;
 		} catch (PersistenceException e) {
-			// TODO: add logging
-			throw e;
+			// TODO: add logging "The user with login: " + user.getLogin() + " is already exists!"
+			throw new DBException("The user with login: " + user.getLogin() + " is already exists!", e);
 		} catch (Exception e) {
 			// TODO: add logging
 			// TODO: add processing case when the user already exists
 			throw new DBException("Cannot add User with login: " + user.getLogin(), e);
 		}
+		return result;
 	}
 
 	@Override
 	public User getByID(long id) throws DBException{
-		User user = new User();
+		User user = null;
 		try(Session session = SESSION_FACTORY.openSession()){
 			session.beginTransaction();
-			session.load(user, id);
-			user.getDivisions().size(); //for attaching the set of divisions
+			user = session.get(User.class, id);
+			if (user != null) {
+				user.getDivisions().size(); //for attaching the set of divisions
+			}
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			// TODO: add logging
@@ -89,27 +99,26 @@ public class UserService implements UserDAO {
 	}
 
 	@Override
-	public List<String> getAllLogins() throws DBException{
-		List<String> userList = null;
+	public Set<String> getAllLogins() throws DBException{
+		Set<String> userSet = null;
 		try (Session session = SESSION_FACTORY.openSession()){
 			session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<String> criteriaQuery = builder.createQuery(String.class);
 			Root<User> userRoot = criteriaQuery.from(User.class);
 			criteriaQuery.select(userRoot.get(User_.login));
-			userList = session.createQuery(criteriaQuery).getResultList();
-			// TODO: Change to set
+			userSet = new HashSet<String>(session.createQuery(criteriaQuery).getResultList());
 			session.getTransaction().commit();
 		}catch (Exception e) {
 			// TODO: add logging in UserService.getAllLogins()
 			throw new DBException("Cannot read logins of all users", e);
 		}
-		return userList;
+		return userSet;
 	}
 	
 	@Override
-	public List<String> getByRole(Role role) throws DBException {
-		List<String> userList = null;
+	public Set<String> getByRole(Role role) throws DBException {
+		Set<String> userSet = null;
 		try (Session session = SESSION_FACTORY.openSession()){
 			session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
@@ -118,51 +127,114 @@ public class UserService implements UserDAO {
 			criteriaQuery.select(userRoot.get(User_.login));
 			Predicate predicate = builder.equal(userRoot.get(User_.role), role);
 			criteriaQuery.where(predicate);
-			userList = session.createQuery(criteriaQuery).getResultList();
-			// TODO: Change to set
+			userSet = new HashSet<String>(session.createQuery(criteriaQuery).getResultList());
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			// TODO: add logging in UserService.getByRole()
 			throw new DBException("Cannot read users with role: " + role.name(), e);
 		}
-		return userList;
+		return userSet;
 	}
 
 	@Override
-	public List<String> getByDevision(String devisionName) throws DBException{
-		// TODO Auto-generated method stub
-		// TODO: Change to set
-		return null;
+	public Set<String> getByDivision(String divisionName) throws DBException{
+		Set<String> userSet = null;
+		try (Session session = SESSION_FACTORY.openSession()){
+			session.beginTransaction();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<Division> criteriaQuery = builder.createQuery(Division.class);
+			Root<Division> divisionRoot = criteriaQuery.from(Division.class);
+			criteriaQuery.select(divisionRoot);
+			criteriaQuery.where(builder.equal(divisionRoot.get(Division_.name), divisionName));
+			Division division = session.createQuery(criteriaQuery).getSingleResult();
+			if (division != null) {
+				userSet = division.getUsers().stream().map(User::getLogin).collect(Collectors.toSet());
+			}
+			session.getTransaction().commit();
+		} catch (NoResultException e) {
+			// TODO: add logging
+			userSet = new HashSet<String>();
+		} catch (Exception e) {
+			// TODO: add logging in UserService.getByDivision()
+			throw new DBException("Cannot read users with divisions: " + divisionName, e);
+		}
+		return userSet;
 	}
 
 	@Override
-	public void deleteByID(long id) throws DBException{
-		// TODO Auto-generated method stub
-
+	public boolean deleteByID(long id) throws DBException {
+		boolean result = false;
+		Transaction transaction = null;
+		try (Session session = SESSION_FACTORY.openSession()) {
+			transaction = session.beginTransaction();
+			User user = session.byId(User.class).load(id);
+			if(user != null) {
+				session.delete(user);
+			}
+			transaction.commit();
+			result = true;
+		} catch (Exception e) {
+			// TODO Add logging
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			throw new DBException("Cannot delet the user with id=" + id, e);
+		}
+		return result;
 	}
 
 	@Override
-	public void deleteByLogin(String login) throws DBException{
-		// TODO Auto-generated method stub
-
+	public boolean deleteByLogin(String login) throws DBException {
+		boolean result = false;
+		Transaction transaction = null;
+		try (Session session = SESSION_FACTORY.openSession()) {
+			transaction = session.beginTransaction();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaDelete<User> deleteQuery = builder.createCriteriaDelete(User.class);
+			/*
+			 * 
+			 * 
+			 * 
+			 * 
+			 * 
+			 * 
+			 */
+			
+			
+			
+			Root<User> userRoot = deleteQuery.from(User.class);
+			deleteQuery.from(User.class);
+			deleteQuery.where(builder.equal(userRoot.get(User_.login), login));
+			session.createQuery(deleteQuery);
+			transaction.commit();
+			result = true;
+		} catch (Exception e) {
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			
+			// TODO: add logging in UserService.getByDivision()
+			throw new DBException("Cannot delete user with login: " + login, e);
+		}
+		return result;
 	}
 
 	@Override
-	public void delete(User user) throws DBException{
+	public boolean delete(User user) throws DBException {
 		// TODO Auto-generated method stub
-
+		return false;
 	}
 
 	@Override
-	public void update(User user) throws DBException{
+	public boolean update(User user) throws DBException {
 		// TODO Auto-generated method stub
-
+		return false;
 	}
 
 	@Override
-	public void addDivision(String login, Division division) throws DBException{
+	public boolean addDivision(String login, Division division) throws DBException {
 		// TODO Auto-generated method stub
-
+		return false;
 	}
 
 }
