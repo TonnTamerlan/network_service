8cfe7680403f7a3f15e87a5ea173582e457d6ce5
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-02-27 18:11:33 +0200

    dbUtil.dao.EquipmentDAO: refactoring
    Changed return type some methods and delete the method getAll

--- src/main/java/dbUtil/dao/EquipmentDAO.java
+++ src/main/java/dbUtil/dao/EquipmentDAO.java
@@ -1,6 +1,6 @@
 package dbUtil.dao;
 
-import java.util.Set;
+import java.util.List;
 
 import dbUtil.DBException;
 import dbUtil.dataSets.Division;
@@ -15,29 +15,22 @@ import dbUtil.dataSets.Equipment;
 public interface EquipmentDAO extends DAO<Equipment> {
 
 	/**
-	 * Return all of entities from the repository
-	 * 
-	 * @return set of all of divisions
-	 */
-	Set<Equipment> getAll() throws DBException;
-	
-	/**
 	 * Find and get all equipments which belong the division and which name is equipName
 	 * 
 	 * @param equipName is equipment's name
 	 * @param div is the division which equipments belong
-	 * @return set of equipments
+	 * @return list of equipments
 	 */
-	public Set<Equipment> getByNameAndDivision(String equipName, Division div)  throws DBException;
+	public List<Equipment> getByNameAndDivision(String equipName, Division div)  throws DBException;
 	
 	/**
 	 * Find and get all equipments which belong the division and which name is equipName
 	 * 
 	 * @param equipName is equipment's name
 	 * @param divName is division's name which equipments belong
-	 * @return set of equipments
+	 * @return list of equipments
 	 */
-	public Set<Equipment> getByNameAndDivision(String equipName, String divName)  throws DBException;
+	public List<Equipment> getByNameAndDivision(String equipName, String divName)  throws DBException;
 	
 	/**
 	 * Get all equipments of the division which name is nameDivision and belongs division which name is nameMasterDevision
@@ -46,15 +39,15 @@ public interface EquipmentDAO extends DAO<Equipment> {
 	 * @param nameMasterDivision is name of division which the division belongs
 	 * @return all the division's equipments
 	 */
-	public Set<Equipment> getByDivision(String nameDivision, String nameMasterDivision)  throws DBException;
+	public List<Equipment> getByDivision(String nameDivision, String nameMasterDivision)  throws DBException;
 	
 	
 	/**
 	 * Find and read all of the equipment which name is equipName
 	 * 
 	 * @param equipName is name of equipments
-	 * @return set of equipments 
+	 * @return list of equipments 
 	 */
-	public Set<Equipment> getAllByName(String equipName)  throws DBException;
+	public List<Equipment> getAllByName(String equipName)  throws DBException;
 	
 }
