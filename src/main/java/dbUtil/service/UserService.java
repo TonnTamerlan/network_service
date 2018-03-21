ee4a3218b3142671318fba0f3c37be5e8c782dc8
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-03-21 23:22:39 +0200

    correct method add and covering it unit test

--- src/main/java/dbUtil/service/UserService.java
+++ src/main/java/dbUtil/service/UserService.java
@@ -48,22 +48,63 @@ public class UserService implements UserDAO {
 
 	@Override
 	public boolean add(User user) throws DBException {
-		LOGGER.debug("Try to add the user \"{}\" with login \"{}\" in repository", user.getLastName(), user.getLogin());
+		LOGGER.debug("Try to add the user {} in the repository", user);
 		boolean result = false;
-		try (Session session = SESSION_FACTORY.openSession()) {
-			session.beginTransaction();
+		if (user == null) {
+			String errorMessage = "The user is null";
+			LOGGER.debug(errorMessage);
+			throw new IllegalArgumentException(errorMessage);
+		}
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
+			transaction = session.beginTransaction();
+			for (Division div : user.getDivisions()) {
+				if (session.get(Division.class, div.getId()) == null) {
+					String errorMessage = "The division " + div + " doesn't exist in the repository";
+					LOGGER.debug(errorMessage);
+					throw new IllegalArgumentException(errorMessage);
+				}
+			}
 			session.save(user);
-			session.getTransaction().commit();
-			LOGGER.debug("The user \"{}\" with login \"{}\" and id={} has added", user.getLastName(), user.getLogin(),
-					user.getId());
+			transaction.commit();
+			LOGGER.debug("The user {} has added", user);
 			result = true;
 		} catch (PersistenceException e) {
-			LOGGER.debug("The user \"{}\" with login \"{}\" already exists!", user.getLastName(), user.getLogin());
-			LOGGER.catching(Level.DEBUG, e);
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction", ignored);
+			}
+			String errorMessage = "The user" + user + " already exists or some its fields are wrong!";
+			LOGGER.error(errorMessage, e);
+			throw new IllegalArgumentException(errorMessage, e);
+		} catch (IllegalArgumentException e) {
+			throw e;
 		} catch (Exception e) {
-			String errorMessage = "Cannot add the user \"" + user.getLastName() + "\" with login \"" + user.getLogin() + "\"";
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction in method add", ignored);
+			}
+			String errorMessage = "Cannot add the user " + user;
 			LOGGER.error(errorMessage, e);
 			throw new DBException(errorMessage, e);
+		} finally {
+			try {
+				session.close();
+				LOGGER.trace("Session is close");
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot close the session", ignored);
+			}
 		}
 		return result;
 	}
@@ -72,7 +113,10 @@ public class UserService implements UserDAO {
 	public User getById(long id) throws DBException {
 		LOGGER.debug("Try to get a user by id={}", id);
 		User user = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		Session session = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			session.beginTransaction();
 			user = session.get(User.class, id);
 			if (user != null) {
@@ -97,7 +141,10 @@ public class UserService implements UserDAO {
 	public User getByLogin(String login) throws DBException {
 		LOGGER.debug("Try to get the user with login=\"{}\"", login);
 		User user = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		Session session = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
@@ -125,7 +172,10 @@ public class UserService implements UserDAO {
 	public Set<String> getAllLogins() throws DBException {
 		LOGGER.debug("Try to get all the user logins");
 		Set<String> userSet = Collections.emptySet();
-		try (Session session = SESSION_FACTORY.openSession()) {
+		Session session = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<String> criteriaQuery = builder.createQuery(String.class);
@@ -146,7 +196,10 @@ public class UserService implements UserDAO {
 	public Set<String> getByRole(Role role) throws DBException {
 		LOGGER.debug("Try to get user names by role \"{}\"", role);
 		Set<String> userSet = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		Session session = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<String> criteriaQuery = builder.createQuery(String.class);
@@ -169,7 +222,10 @@ public class UserService implements UserDAO {
 	public Set<String> getByDivision(String divisionName) throws DBException {
 		LOGGER.debug("Try to get user logins by the division name \"{}\"", divisionName);
 		Set<String> userSet = Collections.emptySet();
-		try (Session session = SESSION_FACTORY.openSession()) {
+		Session session = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<Division> criteriaQuery = builder.createQuery(Division.class);
@@ -197,8 +253,11 @@ public class UserService implements UserDAO {
 	public boolean deleteById(long id) throws DBException {
 		LOGGER.debug("Try to delete the user by id={}", id);
 		boolean result = false;
+		Session session = null;
 		Transaction transaction = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			transaction = session.beginTransaction();
 			User user = session.byId(User.class).load(id);
 			if (user != null) {
@@ -212,12 +271,24 @@ public class UserService implements UserDAO {
 				LOGGER.debug("The user with id={} does not exist", id);
 			}
 		} catch (Exception e) {
-			if (transaction != null && transaction.isActive()) {
-				transaction.rollback();
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction in method add", ignored);
 			}
 			String errorMessage = "Cannot delete the user with id=" + id;
 			LOGGER.error(errorMessage, e);
 			throw new DBException(errorMessage, e);
+		} finally {
+			try {
+				session.close();
+				LOGGER.trace("Session is close");
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot close the session", ignored);
+			}
 		}
 		return result;
 	}
@@ -226,8 +297,11 @@ public class UserService implements UserDAO {
 	public boolean deleteByLogin(String login) throws DBException {
 		LOGGER.debug("Try to delete a user by login=\"{}\"", login);
 		boolean result = false;
+		Session session = null;
 		Transaction transaction = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			transaction = session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaDelete<User> deleteQuery = builder.createCriteriaDelete(User.class);
@@ -242,12 +316,24 @@ public class UserService implements UserDAO {
 				LOGGER.debug("The user with login \"{}\" doesn't exist", login);
 			}
 		} catch (Exception e) {
-			if (transaction != null && transaction.isActive()) {
-				transaction.rollback();
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction in method add", ignored);
 			}
 			String errorMessage = "Cannot delete user with login: " + login;
 			LOGGER.error(errorMessage, e);
 			throw new DBException(errorMessage, e);
+		} finally {
+			try {
+				session.close();
+				LOGGER.trace("Session is close");
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot close the session", ignored);
+			}
 		}
 		return result;
 	}
@@ -256,8 +342,11 @@ public class UserService implements UserDAO {
 	public boolean delete(User user) throws DBException {
 		LOGGER.debug("Try to delete the user {}", user);
 		boolean result = false;
+		Session session = null;
 		Transaction transaction = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			transaction = session.beginTransaction();
 			if (session.get(User.class, user.getId()) != null) {
 				session.clear();
@@ -270,12 +359,24 @@ public class UserService implements UserDAO {
 				LOGGER.debug("The user {} didn't find", user);
 			}
 		} catch (Exception e) {
-			if (transaction != null && transaction.isActive()) {
-				transaction.rollback();
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction in method add", ignored);
 			}
 			String errorMessage = "Cannot delete the user " + user;
 			LOGGER.error(errorMessage, e);
 			throw new DBException(errorMessage, e);
+		} finally {
+			try {
+				session.close();
+				LOGGER.trace("Session is close");
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot close the session", ignored);
+			}
 		}
 		return result;
 	}
@@ -285,8 +386,11 @@ public class UserService implements UserDAO {
 		LOGGER.debug("Try to update the user \"{}\" with login \"{}\" and id={}",
 				user.getLastName(), user.getLogin(), user.getId());
 		boolean result = false;
+		Session session = null;
 		Transaction transaction = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
 			transaction = session.beginTransaction();
 			session.update(user);
 			transaction.commit();
@@ -294,10 +398,25 @@ public class UserService implements UserDAO {
 					user.getLastName(), user.getLogin(), user.getId());
 			result = true;
 		} catch (Exception e) {
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction in method add", ignored);
+			}
 			String errorMessage = "Cannot update the user \"" + user.getLastName() +
 					"\" with login \"" + user.getLogin() + "\" and id=" + user.getId();
 			LOGGER.error(errorMessage , e);
 			throw new DBException(errorMessage , e);
+		} finally {
+			try {
+				session.close();
+				LOGGER.trace("Session is close");
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot close the session", ignored);
+			}
 		}
 		return result;
 	}
@@ -307,9 +426,12 @@ public class UserService implements UserDAO {
 		LOGGER.debug("Try to add the division \"{}\" with id={} in the user with login \"{}\"", division.getName(),
 				division.getId(), login);
 		boolean result = false;
-		Transaction transanction = null;
-		try (Session session = SESSION_FACTORY.openSession()) {
-			transanction = session.beginTransaction();
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = SESSION_FACTORY.openSession();
+			LOGGER.trace("Session is open");
+			transaction = session.beginTransaction();
 			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
 			Root<User> userRoot = criteriaQuery.from(User.class);
@@ -318,7 +440,7 @@ public class UserService implements UserDAO {
 			criteriaQuery.where(predicate);
 			User user = session.createQuery(criteriaQuery).getSingleResult();
 			user.addDivision(division);
-			transanction.commit();
+			transaction.commit();
 			result = true;
 			LOGGER.debug("The division \"{}\" with id={} was added in the user \"{}\" with login \"{}\" and id={}",
 					division.getName(), division.getId(), user.getLastName(), user.getLogin(), user.getId());
@@ -326,13 +448,25 @@ public class UserService implements UserDAO {
 			LOGGER.debug("The user with login=\"{}\" doesn't exist", login);
 			LOGGER.catching(Level.DEBUG, e);
 		} catch (Exception e) {
-			if (transanction != null && transanction.isActive()) {
-				transanction.rollback();
+			try {
+				if (transaction != null && transaction.isActive()) {
+					transaction.rollback();
+					LOGGER.trace("The transaction rollbacked");
+				}
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot rollback transaction in method add", ignored);
 			}
 			String errorMessage = "Cannot to add the division \"" + division.getName() + "\" with id=" + division.getId()
 					+ " to the user with login \"" + login + "\"";
 			LOGGER.error(errorMessage, e);
 			throw new DBException(errorMessage, e);
+		} finally {
+			try {
+				session.close();
+				LOGGER.trace("Session is close");
+			} catch (Exception ignored) {
+				LOGGER.error("Cannot close the session", ignored);
+			}
 		}
 		return result;
 	}
--- src/test/java/dbUtil/service/UserServiceTest.java
+++ src/test/java/dbUtil/service/UserServiceTest.java
@@ -1,18 +1,25 @@
 package dbUtil.service;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import dbUtil.DBException;
+import dbUtil.dao.DivisionDAO;
 import dbUtil.dao.UserDAO;
+import dbUtil.dataSets.Division;
 import dbUtil.dataSets.Role;
 import dbUtil.dataSets.User;
 
@@ -20,12 +27,14 @@ class UserServiceTest {
 	
 	private static SessionFactory sessionFactory = null;
 	private static UserDAO userService = null;
+	private static DivisionDAO divisionService = null;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		Configuration cfg = new Configuration().configure("test_hibernate.cfg.xml");
 		sessionFactory = cfg.buildSessionFactory();
 		userService = new UserService(sessionFactory);
+		divisionService = new DivisionService(sessionFactory);
 	}
 
 	@AfterAll
@@ -44,8 +53,60 @@ class UserServiceTest {
 	}
 
 	@Test
-	void testAdd() {
-		fail("Not yet implemented");
+	@DisplayName("Adding the user in the repository")
+	void testAdd() throws DBException {
+		String userPefix = "Add_";
+		String divisionPrefix = "UserServiceAdd_";
+		
+		//Testing adding null. The method must throw IllegalArgumentException
+		User nullUser = null;
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> userService.add(nullUser));
+		assertEquals("The user is null", exception.getMessage());
+		
+		//Testing adding the user with wrong parameters
+		User userWithLoginNull = createExampleUser(null, Role.ADMIN);
+		exception = assertThrows(IllegalArgumentException.class, ()->userService.add(userWithLoginNull));
+		assertEquals("The user" + userWithLoginNull + " already exists or some its fields are wrong!", 
+				exception.getMessage());
+		User userWithPasswordNull = createExampleUser("User_password_null", Role.ADMIN);
+		userWithPasswordNull.setPassword(null);
+		exception = assertThrows(IllegalArgumentException.class, ()->userService.add(userWithPasswordNull));
+		assertEquals("The user" + userWithPasswordNull + " already exists or some its fields are wrong!", 
+				exception.getMessage());
+		User userWithRollNull = createExampleUser("User_role_null", null);
+		exception = assertThrows(IllegalArgumentException.class, ()->userService.add(userWithRollNull));
+		assertEquals("The user" + userWithRollNull + " already exists or some its fields are wrong!", 
+				exception.getMessage());
+		
+		//Testing adding the user with a division that doesn't exist in the repository
+		//The method must throw IllegalArgumentException
+		Division oneDivision = DivisionServiceTest.createExampleDivision(divisionPrefix + 1);
+		User userWithNotexistingDivision = createExampleUser("User_Division_not_exist", Role.USER);
+		userWithNotexistingDivision.addDivision(oneDivision);
+		exception = assertThrows(IllegalArgumentException.class, ()->userService.add(userWithNotexistingDivision));
+		assertEquals("The division " + oneDivision + " doesn't exist in the repository", 
+				exception.getMessage());
+		
+		//Testing adding the user that doesn't exist in the repository and all fields is correct
+		assumeTrue(divisionService.add(oneDivision));
+		User oneUser = createExampleUser(userPefix + 1, Role.ADMIN);
+		oneUser.addDivision(oneDivision);
+		assertTrue(userService.add(oneUser));
+		assertTrue(divisionService.getById(oneDivision.getId()).getUsers().contains(oneUser));
+		
+		//Testing adding the user when it already exists in the repository. Method must throw IllegalArgumentException
+		exception = assertThrows(IllegalArgumentException.class, ()->userService.add(oneUser));
+		assertEquals("The user" + oneUser + " already exists or some its fields are wrong!", 
+				exception.getMessage());
+		
+		//Testing adding the user with the login that already exists in the repository.
+		//Method must throw IllegalArgumentException
+		User userWithSameLogin = createExampleUser("User_same_login", Role.ADMIN);
+		userWithSameLogin.setLogin(oneUser.getLogin());
+		exception = assertThrows(IllegalArgumentException.class, ()->userService.add(userWithSameLogin));
+		assertEquals("The user" + userWithSameLogin + " already exists or some its fields are wrong!", 
+				exception.getMessage());
+		
 	}
 
 	@Test
@@ -100,11 +161,13 @@ class UserServiceTest {
 	
 	
 	@SuppressWarnings("unchecked")
-	private static void deleteAllUsers() {
+	private static void clearRepository() {
 		try (Session session = sessionFactory.openSession()) {
 			session.beginTransaction();
-			Query<Integer> query = session.createQuery("DELETE FROM User");
-			query.executeUpdate();
+			session.createQuery("DELETE FROM Equipment").executeUpdate();
+			session.createQuery("DELETE FROM Division").executeUpdate();
+			session.createQuery("DELETE FROM User").executeUpdate();
+			session.createNativeQuery("DELETE FROM user_division").executeUpdate();
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			throw e;
