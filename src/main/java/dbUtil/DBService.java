0740400a50ca2a7202a784c6c6848fdd86f5e0c9
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-03-28 22:57:38 +0300

    add modifier static to some method

--- src/main/java/dbUtil/DBService.java
+++ src/main/java/dbUtil/DBService.java
@@ -10,39 +10,36 @@ public class DBService {
 	
 	final static Logger LOGGER = LogManager.getLogger(DBService.class.getName());
 	
-	private SessionFactory sessionFactory = null;
-	private String CFG_RESOURCE_NAME = "hibernate.cfg.xml";
+	private static SessionFactory sessionFactory = null;
+	private static String CFG_RESOURCE_NAME = "hibernate.cfg.xml";
 	
 	public DBService() {
-		this.start();
+		start();
 		LOGGER.debug("Data base service created and started");
 	}
 
-	public void stop() {
-		if(this.isWorked()) {
+	public static void stop() {
+		if(isWorked()) {
 			sessionFactory.close();
 		}
 		LOGGER.debug("Data base service is stopped");
 	}
 
-	public boolean isWorked() {
+	public static boolean isWorked() {
 		return sessionFactory != null && sessionFactory.isOpen();
 	}
 
-	public void start() {
+	public static SessionFactory start() {
 		if(sessionFactory == null || sessionFactory.isClosed()) {
 			Configuration cfg = new Configuration().configure(CFG_RESOURCE_NAME);
 			sessionFactory = cfg.buildSessionFactory();
 		}
 		LOGGER.debug("Data base service is started");
-	}
-
-	public SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
 	}
-	
+
 }
