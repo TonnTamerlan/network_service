a06b9a235c60fd8b4f4c743adb42198d81ab6e34
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-03-29 23:51:43 +0300

    changed to singleton

--- src/main/java/dbUtil/DBService.java
+++ src/main/java/dbUtil/DBService.java
@@ -5,31 +5,44 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import dbUtil.dao.DivisionDAO;
+import dbUtil.dao.EquipmentDAO;
+import dbUtil.dao.UserDAO;
+import dbUtil.service.DivisionService;
+import dbUtil.service.EquipmentService;
+import dbUtil.service.UserService;
+
 
 public class DBService {
 	
 	final static Logger LOGGER = LogManager.getLogger(DBService.class.getName());
 	
-	private static SessionFactory sessionFactory = null;
-	private static String CFG_RESOURCE_NAME = "hibernate.cfg.xml";
+	final private static DBService dbService = new DBService();
+	private SessionFactory sessionFactory = null;
+	private String CFG_RESOURCE_NAME = "hibernate.cfg.xml";
 	
-	public DBService() {
+	
+	private DBService() {
 		start();
 		LOGGER.debug("Data base service created and started");
 	}
 
-	public static void stop() {
+	public static DBService getInstance() {
+		return dbService;
+	}
+	
+	public void stop() {
 		if(isWorked()) {
 			sessionFactory.close();
 		}
 		LOGGER.debug("Data base service is stopped");
 	}
 
-	public static boolean isWorked() {
+	public boolean isWorked() {
 		return sessionFactory != null && sessionFactory.isOpen();
 	}
 
-	public static SessionFactory start() {
+	public SessionFactory start() {
 		if(sessionFactory == null || sessionFactory.isClosed()) {
 			Configuration cfg = new Configuration().configure(CFG_RESOURCE_NAME);
 			sessionFactory = cfg.buildSessionFactory();
@@ -37,9 +50,17 @@ public class DBService {
 		LOGGER.debug("Data base service is started");
 		return sessionFactory;
 	}
-
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
+	
+	public UserDAO getUserService() {
+		return new UserService(start());
+	}
+	
+	public DivisionDAO getDivisionService() {
+		return new DivisionService(start());
+	}
+	
+	public EquipmentDAO getEquipmentService() {
+		return new EquipmentService(start());
 	}
 
 }
